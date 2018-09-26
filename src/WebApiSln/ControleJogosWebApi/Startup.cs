4b8763f59390dfa03ca849b54e2732eb688972aa
Daniel Maack <danielmaack@cwi.com.br>
2018-09-26 19:09:44 -0300

    Commit

--- src/WebApiSln/ControleJogosWebApi/Startup.cs
+++ src/WebApiSln/ControleJogosWebApi/Startup.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Repository.Entity;
+using Repository;
 
 namespace ControleJogosWebApi
 {
@@ -20,10 +19,9 @@ namespace ControleJogosWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-			services.AddDbContext<WebApiDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            
+            services
+                .AddRepository(configuration: Configuration)
+                .AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
